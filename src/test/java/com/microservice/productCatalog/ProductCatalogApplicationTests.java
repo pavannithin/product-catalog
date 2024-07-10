e98eea340f53516a30378683609a0df05c749194
pavannithin <nithin.pavan@gmail.com>
2024-07-11 00:11:48 +0530

    test cases added and tested

--- src/test/java/com/microservice/productCatalog/ProductCatalogApplicationTests.java
+++ src/test/java/com/microservice/productCatalog/ProductCatalogApplicationTests.java
@@ -1,19 +1,19 @@
 package com.microservice.productCatalog;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microservice.productCatalog.dto.ProductRequest;
+import com.microservice.productCatalog.repos.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Repository;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -38,6 +38,9 @@ class ProductCatalogApplicationTests {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private ProductRepository productRepository;
+
 
     @Test
     void shouldCreateProduct() throws Exception {
@@ -47,6 +50,7 @@ class ProductCatalogApplicationTests {
         String stringResponse = objectMapper.writeValueAsString(productRequest);
         ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/api/product").contentType(MediaType.APPLICATION_JSON)
                 .content(stringResponse)).andExpect(status().isCreated());
+        Assertions.assertTrue( productRepository.findAll().size() > 0);
     }
 
     private ProductRequest getProductRequest() {
